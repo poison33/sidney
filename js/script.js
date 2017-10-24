923674185a58486776a9c0b867523850cea4f490
poison33 <poison_560@bk.ru>
2017-10-24 18:17:17 +0300

    23

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
-// JavaScript Document
+/* JavaScript Document*/
 jQuery(document).ready(function(){
-	
+	jQuery('i.fa-bars, div.navbar-collapse').hover(function(){
+		jQuery('div.navbar-collapse').addClass('hidden-menu');
+	}, function(){
+		jQuery('div.navbar-collapse').removeClass('hidden-menu');
+	});
 });
